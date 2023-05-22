0f6da96d4405f3f18c132619b27300a4414cb8bb
yanzhang <728308386@qq.com>
2023-05-22 09:56:54 +0800

    改用soundpool来播放背景音乐，解决了pixel 3a虚拟机上的崩溃问题。 目前还没有测试在三星手机上会怎么样

--- app/src/main/java/com/example/spacewar/MainActivity.kt
+++ app/src/main/java/com/example/spacewar/MainActivity.kt
@@ -1,50 +1,48 @@
 package com.example.spacewar
 
-import android.media.MediaPlayer
+import android.media.AudioAttributes
+import android.media.SoundPool
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var gameView: GameView
-    private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var soundPool: SoundPool
+    private var backgroundMusic: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         gameView = GameView(this)
         setContentView(gameView)
 
-        mediaPlayer = MediaPlayer.create(this, R.raw.background)
-        mediaPlayer.isLooping = true // 设置为循环播放
-        mediaPlayer.start() // 开始播放
+        val audioAttributes = AudioAttributes.Builder()
+            .setUsage(AudioAttributes.USAGE_GAME)
+            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+            .build()
+
+        soundPool = SoundPool.Builder()
+            .setAudioAttributes(audioAttributes)
+            .setMaxStreams(1)
+            .build()
+
+        backgroundMusic = soundPool.load(this, R.raw.background, 1)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        gameView.requestFocus()
+        soundPool.setOnLoadCompleteListener { _, _, _ ->
+            soundPool.play(backgroundMusic, 1f, 1f, 0, -1, 1f)
+        }
     }
 
     override fun onPause() {
         super.onPause()
-        mediaPlayer.pause() // 暂停音乐
-    }
-
-    override fun onStop() {
-        super.onStop()
-        mediaPlayer.stop() // 停止音乐
-    }
-    override fun onResume() {
-        super.onResume()
-        gameView.requestFocus()
-        if (!mediaPlayer.isPlaying) {
-            mediaPlayer.start() // 重新开始播放
-        }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        if (!mediaPlayer.isPlaying) {
-            mediaPlayer.start() // 重新开始播放
-        }
+        soundPool.autoPause()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         gameView.stop()
-        mediaPlayer.release() // 释放MediaPlayer占用的资源
     }
 }
