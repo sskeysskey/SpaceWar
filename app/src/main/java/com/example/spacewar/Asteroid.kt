df0e0e6f214238f4dc89cd11ca50c623547c7bbe
yanzhang <728308386@qq.com>
2023-05-23 11:55:23 +0800

    增加陨石上绘制血量——小港提的需求

--- app/src/main/java/com/example/spacewar/Asteroid.kt
+++ app/src/main/java/com/example/spacewar/Asteroid.kt
@@ -9,6 +9,8 @@ import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.content.res.AppCompatResources
 import java.util.Timer
 import java.util.TimerTask
+import android.graphics.Paint
+import android.graphics.Color
 
 class Asteroid(private val bitmap: Bitmap, var x: Float, var y: Float, var health: Int) {
     private val speed = 5f
@@ -16,6 +18,12 @@ class Asteroid(private val bitmap: Bitmap, var x: Float, var y: Float, var healt
     private val height = bitmap.height
     private var rotation = 0f
     private val matrix = Matrix()
+    //以下为绘制陨石上血量显示功能
+    private val healthPaint = Paint().apply {
+        textSize = 40f  // 根据你的需要调整文本大小
+        color = Color.WHITE  // 你可以选择其他颜色来表示血量
+    }
+    //绘制结束
 
     val boundingBox: RectF
         get() = RectF(x, y, x + width, y + height)
@@ -31,6 +39,13 @@ class Asteroid(private val bitmap: Bitmap, var x: Float, var y: Float, var healt
         matrix.postRotate(rotation) // rotate the bitmap
         matrix.postTranslate(x + bitmap.width / 2f, y + bitmap.height / 2f) // lastly move the bitmap to its desired location
         canvas.drawBitmap(bitmap, matrix, null)
+        // 以下为陨石上绘制血量
+        val healthText = health.toString()
+        val textWidth = healthPaint.measureText(healthText)
+        val textX = x + bitmap.width - textWidth - 10 // 将血量放在陨石右下角，离陨石边缘10个像素
+        val textY = y + bitmap.height - 10 // 同上
+        canvas.drawText(healthText, textX, textY, healthPaint)
+        //绘制结束
     }
 }
 
